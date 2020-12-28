fb78a1289c5449eb8d9b7c5b2af0abd3a27854b6
Malik <malikderkaoui@gmail.com>
2020-12-28 18:55:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Landing from './components/Landing/Landing';
 
-import './App.css';
 function App() {
   return (
     <div className="App">
