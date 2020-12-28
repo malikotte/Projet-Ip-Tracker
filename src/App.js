78ff9c96fec733e92d8250297d33a24a20283a51
Malik <malikderkaoui@gmail.com>
2020-12-28 18:51:44 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Landing from './components/Landing/Landing';
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
